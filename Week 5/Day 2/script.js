2309d8bc3168217d4c451c09b0d5446a9e497cb3
Josh Evans <88986229+Jevans1441@users.noreply.github.com>
2021-10-21 22:26:27 -0400

    Update script.js

--- Week 5/Day 2/script.js
+++ Week 5/Day 2/script.js
@@ -4,11 +4,19 @@ const listItemOne = document.createElement("li");
 const listItemTwo = document.createElement("li");
 const listItemThree = document.createElement("li");
 const test = document.querySelector("#test");
-
+const divElement = document.createElement("div");
+const paragraphElement = document.createElement("p");
 //call the test element and add created elements
+
+divElement.id = "container";
+paragraphElement.id = "paraHome";
+
+//Calling elements to make id's
 
 test.append(headerElement);
 test.append(listElement);
+test.append(divElement);
+divElement.appendChild(paragraphElement);
 listElement.appendChild(listItemOne);
 listElement.appendChild(listItemTwo);
 listElement.appendChild(listItemThree);
@@ -18,3 +26,6 @@ listElement.appendChild(listItemThree);
 headerElement.innerText = "This is my header";
 headerElement.style = "color: blue";
 headerElement.align = "center";
+
+paragraphElement.innerText =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla at est odio. Suspendisse commodo, leo vel dapibus vestibulum, nisl odio imperdiet justo, id euismod purus eros quis felis. In ligula turpis, vehicula in imperdiet vel, faucibus non lorem. Praesent elementum odio nec felis lacinia, in viverra risus dignissim. Vestibulum aliquet lobortis scelerisque. Nam egestas consectetur odio, eget varius dui pulvinar in. Cras molestie suscipit arcu ac interdum. Pellentesque mattis aliquam nulla, ut venenatis elit sollicitudin et. Suspendisse potenti. Donec gravida felis et ex placerat facilisis. Aenean accumsan consectetur sapi";

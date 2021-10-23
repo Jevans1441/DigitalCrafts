dc8a990d39444ef53bc0b56caeed516f7ed8f781
Josh Evans <88986229+Jevans1441@users.noreply.github.com>
2021-10-23 12:24:52 -0400

    Update classExercise.js

--- Week 5/Day 3/classExercise.js
+++ Week 5/Day 3/classExercise.js
@@ -10,8 +10,6 @@
 //   }
 // }
 
-
-
 // let myArray = ["apple", "bread", "eggs"];
 
 // for (let item = 0; item < myArray.length; item++) {
@@ -25,4 +23,23 @@
 // let firstTwoElements = myArray.slice(0, 2);
 // // console.log(firstTwoElements);
 
+// let numbers = [1, 2, 3, 4, 5, 6];
 
+// console.log("My array before: " + numbers);
+
+// numbers.push(7);
+
+// console.log("My array after: " + numbers);
+
+// numbers.splice(2, 1); // removes the number starting at the 2nd index and only the 2nd index (2, 1), it removes number 3
+
+// console.log("My array after splice: " + numbers);
+
+let friends = ["katie", "todd", "josh", "fred"];
+
+for (let friend = 0; friend < friends.length; friend++) {
+  console.log(friends[friend]);
+}
+
+// let withoutKatie = friends.slice(1, 4);
+// console.log(withoutKatie);

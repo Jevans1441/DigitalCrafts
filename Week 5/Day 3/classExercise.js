8e4cd8d78429dafd11254b0d9ba29fb8b37693d8
Josh Evans <88986229+Jevans1441@users.noreply.github.com>
2021-10-23 11:40:18 -0400

    Create classExercise.js

---
+++ Week 5/Day 3/classExercise.js
@@ -0,0 +1,28 @@
+// let count = 0;
+
+// while (count <= 10) {
+//   count++;
+
+//   if (count % 2 == 0) {
+//     console.log(count + " is even");
+//   } else {
+//     console.log(count + " is odd");
+//   }
+// }
+
+
+
+// let myArray = ["apple", "bread", "eggs"];
+
+// for (let item = 0; item < myArray.length; item++) {
+//   //   console.log(myArray[item]);
+//   //   console.log(item);
+// }
+
+// let lastElement = myArray[myArray.length - 1];
+// // console.log(lastElement);
+
+// let firstTwoElements = myArray.slice(0, 2);
+// // console.log(firstTwoElements);
+
+
